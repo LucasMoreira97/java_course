7c0ee3523ee960060910f4d6765aeeb2339f7180
Lucas Moreira <l-moreira@outlook.com.br>
2023-02-05 22:14:08 -0300

    Lesson 112 and 113, about date time formatter

--- lesson_110_date/src/application/Program.java
+++ lesson_110_date/src/application/Program.java
@@ -1,25 +1,24 @@
 package application;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
-		//https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+
+		// https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		//Date formatting examples lesson 111
+		// Date formatting examples lesson 111
 		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
 		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
-		
-		
-		
+
 		LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 = LocalDateTime.now();
 		Instant d03 = Instant.now();
@@ -31,8 +30,8 @@ public class Program {
 		LocalDateTime d09 = LocalDateTime.parse("20/07/2022 19:35", fmt2);
 		LocalDate d10 = LocalDate.of(2022, 7, 20);
 		LocalDateTime d11 = LocalDateTime.of(2022, 7, 20, 1, 30);
-		
 
+		System.out.println("----------------lesson 110-----------------------------");
 		System.out.println("d01 = " + d01);
 		System.out.println("d02 = " + d02);
 		System.out.println("d03 = " + d03);
@@ -44,20 +43,72 @@ public class Program {
 		System.out.println("d09 = " + d09);
 		System.out.println("d10 = " + d10);
 		System.out.println("d11 = " + d11);
-		
-		
-		
-		
-		//Date formatting examples lesson 111
+
+		System.out.println("----------------lesson 111-----------------------------");
+		// Date formatting examples lesson 111
 		System.out.println("d01 - formatted date= " + d01.format(fmt1));
 		System.out.println("d05 - formatted date= " + d05.format(fmt2));
-		
+
 		System.out.println("d06 - formatted date= " + fmt3.format(d06));
 		System.out.println("d05 - ISO formatted date= " + fmt4.format(d05));
+
+		System.out.println("----------------lesson 112-----------------------------");
+		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+
+		System.out.println("r1 = " + r1);
+		System.out.println("r2 = " + r2);
+		System.out.println("r3 = " + r3);
+		System.out.println("r4 = " + r4);
+
+		System.out.println("d04 dia = " + d04.getDayOfMonth());
+		System.out.println("d04 mês = " + d04.getMonthValue());
+		System.out.println("d04 ano = " + d04.getYear());
+
+		System.out.println("d05 hora = " + d05.getHour());
+		System.out.println("d05 minuto = " + d05.getMinute());
+
+		
+		
+		System.out.println("----------------lesson 113-----------------------------");
+	
+		
+		LocalDate pastWeekLocalDate = d04.minusDays(7);
+		LocalDate nextWeekLocaleDate = d04.plusDays(7);
+		
+		System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+		System.out.println("nextWeekLocaleDate = " + nextWeekLocaleDate);
+		
+		
+		LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+		LocalDateTime nextWeekLocaleDateTime = d05.plusDays(7);
+		
+		System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+		System.out.println("nextWeekLocaleDateTime = " + nextWeekLocaleDateTime);
 		
 		
 		
-		System.out.println("-------------------------------------------------");
+		Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+		Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+		
+		
+		System.out.println("pastWeekInstant = " + pastWeekInstant);
+		System.out.println("nextWeekInstant = " + nextWeekInstant);
+		
+		
+		//Duration calculation between dates/times
+		Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+		Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+		Duration t3 = Duration.between(pastWeekInstant, d06);
+		Duration t4 = Duration.between(d06, pastWeekInstant);
+		
+		System.out.println("t1 dias = " + t1.toDays());
+		System.out.println("t2 dias = " + t2.toDays());
+		System.out.println("t3 dias = " + t3.toDays());
+		System.out.println("t4 dias = " + t4.toDays());
+		
 		
 		
 		
