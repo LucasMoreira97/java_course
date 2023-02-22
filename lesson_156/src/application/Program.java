afbdc815fd26e36dd3f004645548746747611619
Lucas Moreira <l-moreira@outlook.com.br>
2023-02-22 16:39:47 -0300

    completion of class 156

---
+++ lesson_156/src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+
+import entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+
+
+
+
+
+    }
+}
